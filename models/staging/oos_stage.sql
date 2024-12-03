597c9b3bc5c7f015a6e96d0497ad6606d3f91fab
Marina Bendersky <marina.bendersky@gmail.com>
2024-12-03 23:15:40 +0000

    Adding a model with staged data (has the correct data types)

--- models/staging/oos_stage.sql
+++ models/staging/oos_stage.sql
@@ -2,47 +2,43 @@ with stage_data as
     (
         select distinct
             event_date
-            , upc
+            , cast(upc as string) as upc
             , COUNT(*) Over (partition by event_date, upc) as cnt
             , dsd
             , workable
             , worked
-            , out_of_stock_duration
-            , hours_recouped
-            , cast(sales_recouped_dollars as float64) as sales_recouped_dollars
-            , cast(expected_workable_loss_dollars as float64) as expected_workable_loss_dollars
+            , cast(hours_recouped as float64) as hours_recouped
+            , cast(out_of_stock_duration as float64) as out_of_stock_duration            
+            , case 
+                when contains_substr(expected_workable_loss_dollars, "-") then 0.0
+                else cast(expected_workable_loss_dollars as float64) 
+                end as expected_workable_loss_dollars
             , cast(expected_loss_dollars as float64) as expected_loss_dollars
             , cast(sales_per_hour_dollars as float64) as sales_per_hour_dollars
+            -- , case
+            --     when contains_substr(sales_recouped_dollars, "-") then 0.0
+            --     else cast(sales_recouped_dollars as float64) 
+            --     end as sales_recouped_dollars
         from {{ ref("out_of_stock") }}
     )
-select distinct
-    event_date
-    , upc
-    , dsd
-    , workable
-    , worked
-   
+select *
+    -- cnt
+    -- , event_date
+    -- , upc
+    -- , dsd
+    -- , workable
+    -- , worked
+    -- , expected_loss_dollars
+    -- , expected_workable_loss_dollars
+    -- , sales_per_hour_dollars
+    -- , sales_recouped_dollars
+    -- , hours_recouped
+    -- , out_of_stock_duration
 from stage_data
--- where upc = 3530100664
-where cnt > 1
--- where expected_workable_loss_dollars = 0
--- where sales_recouped_dollars <= 0
--- where workable=false
--- order by actual_loss desc
+order by event_date, upc
 
+-- code to make sure all data types are correct
+-- removing the column expected_out_of_stock_duration
 
-
-
--- not using the one that has two events for the same day 
--- 2019-01-20
--- 3530100664
-
--- sales recouped is always positive
-
--- in either case, calculate total recouped/gain per day+product (this is what
--- I'll plot)
--- calculate actual_loss
-
-
--- compare actual_loss when workable=true VS a metric when workable was false
--- , expected_loss_dollars-sales_recouped_dollars as actual_loss
+-- next problem: recognizing/eliminating the - in sales_recouped_dollars
+-- after this, data should be OK to just plot it
