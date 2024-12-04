f4c89beed597dbc0eeb819ed1d901dbcd747235e
Marina Bendersky <marina.bendersky@gmail.com>
2024-12-04 18:49:54 +0000

    Added case to cast sales_recouped_dollars as float (instead of string).

--- models/staging/oos_stage.sql
+++ models/staging/oos_stage.sql
@@ -15,10 +15,11 @@ with stage_data as
                 end as expected_workable_loss_dollars
             , cast(expected_loss_dollars as float64) as expected_loss_dollars
             , cast(sales_per_hour_dollars as float64) as sales_per_hour_dollars
-            -- , case
-            --     when contains_substr(sales_recouped_dollars, "-") then 0.0
-            --     else cast(sales_recouped_dollars as float64) 
-            --     end as sales_recouped_dollars
+            , case
+                when regexp_contains(sales_recouped_dollars, r'[()-]') then 0.0
+                else cast(sales_recouped_dollars as float64) 
+                end as sales_recouped_dollars
+
         from {{ ref("out_of_stock") }}
     )
 select *
@@ -39,6 +40,3 @@ order by event_date, upc
 
 -- code to make sure all data types are correct
 -- removing the column expected_out_of_stock_duration
-
--- next problem: recognizing/eliminating the - in sales_recouped_dollars
--- after this, data should be OK to just plot it
